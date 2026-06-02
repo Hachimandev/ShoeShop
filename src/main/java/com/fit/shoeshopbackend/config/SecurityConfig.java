f649c5c30d2960346078ddc3ed5ad175459d10d1
hunh Liem <liemhuynh0789@gmail.com>
2026-06-03 01:04:15 +0700

    Allow /error endpoint

--- src/main/java/com/fit/shoeshopbackend/config/SecurityConfig.java
+++ src/main/java/com/fit/shoeshopbackend/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
                         
                         .requestMatchers("/api/ai/**").permitAll()
                         .requestMatchers("/api/payment/**").permitAll() // Cho phép Webhook SePay truy cập không cần token
-                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll().requestMatchers("/error").permitAll()
 
                         .anyRequest().authenticated()
                 );
